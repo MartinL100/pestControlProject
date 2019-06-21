57c0d0992d02d9de20c3184d439fe6b7dc31cd84
bubblehead121 <2100803857@qq.com>
2019-06-21 10:50:29 +0800

    无操作

--- webModule/src/main/java/com/AAAAAA/pestcontrolproject/servlet/LoginServlet.java
+++ webModule/src/main/java/com/AAAAAA/pestcontrolproject/servlet/LoginServlet.java
@@ -12,7 +12,6 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        doPost(request,response);
+        doPost(request, response);
     }
 }
